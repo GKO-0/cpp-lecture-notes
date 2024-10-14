7cf5160e599f6a21f84615a75f906d0a0bef398e
GKO-0 <gko12@terpmail.umd.edu>
2024-10-14 00:03:53 +0000

    new file:   .vscode/tasks.json new file:   Studio 1/cpp_s1p1 	new file:   Studio 1/cpp_s1p1.cpp 	new file:   Studio 1/cpp_s1p2 	new file:   Studio 1/cpp_s1p2.cpp 	new file:   Studio 1/cpp_s1p3 	new file:   Studio 1/cpp_s1p3.cpp

---
+++ Studio 1/cpp_s1p3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    a = 10;
+    b = 3;
+    cout << "a+b = " << a+b;
+    cout << ", 2*b = " << 2*b << endl;
+    cout << "a*b = " << a*b << endl;
+    return 0; 
+}
