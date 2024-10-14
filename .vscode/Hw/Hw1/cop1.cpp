b48789e0b628ceb50e0650205e1d7b896d80082d
GKO-0 <gko12@terpmail.umd.edu>
2024-10-14 00:41:36 +0000

    new file:   .vscode/Hw/Hw1/cop1 new file:   .vscode/Hw/Hw1/cop1.cpp new file:   .vscode/Hw/Hw1/hw1 new file:   .vscode/Hw/Hw1/hw1.cpp

---
+++ .vscode/Hw/Hw1/cop1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int x1 = -10, x2 = 10, C, n, xg, fg;
+
+    cout << "Enter the value of C: " ;
+    cin >> C;
+    cout << "Number of iterations: ";
+    cin >> n;
+    int f1 = x1^3 + C*x1 -10;
+    int f2 = x2^3 + C*x2 -10;
+    
+    while ( (x2-x1) > 10^-6) {
+
+    xg = (x1/x2)/2;
+    fg = xg^3 + C*xg -10;
+    if (fg < 0) {
+        x1 = xg;
+    } else if (fg > 0) {
+        x2 = xg;
+    }
+    }
+
+    cout << "Found root x to be between " << fg << "and " << fg;
+    return 0;
+}
---
+++ .vscode/Hw/Hw1/hw1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    float c;
+    cout << "Enter the value for a, b, and c: ";
+    cin >> a >> b >> c;
+    cout << "a = " << a << ", b = " << b << ", c = " << c << endl;
+    cout << "a*b = " << a*b << endl;
+    cout << "a/b = " << static_cast<float>(a)/b << endl;
+    cout << "a+b+c = " << a+b+c << endl;
+    return 0;
+}
